99732e860abae68a2e9579a6b00058400cb9314b
Mirabelle Feng <mirabellefeng@gmail.com>
2025-10-26 20:34:06 -0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
+//import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
-    tanstackRouter({
-      target: 'react',
-      autoCodeSplitting: true,
-    }),
+    // tanstackRouter({
+    //   target: 'react',
+    //   autoCodeSplitting: true,
+    // }),
     react(),
     tailwindcss(),
     // ...
