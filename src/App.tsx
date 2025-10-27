9222ee8aea14b2d43f9cc2f502cb674a8ba0c4af
Mirabelle Feng <mirabellefeng@gmail.com>
2025-10-26 20:32:51 -0400

    typecase fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./globals.css";
-import Header from "./components/header/header";
-import Footer from "./components/footer/footer";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 import { Outlet } from "@tanstack/react-router"; // 1. Import Outlet
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
-      <div className="flex justify-center pt-24 pb-18 bg-black">
+      <div className="flex justify-center pt-24 pb-20 bg-black">
         <Outlet />
       </div>
       <Footer />
